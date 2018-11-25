80f0f3bc96fcfe031ed749b4b9627510f0f10190
ndidenko <5773480@gmail.com>
2018-11-25 18:35:05 +0200

    added chat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+password = '5956861'
+1.upto(5) do |i|
+  User.create(
+      email: "user-#{i}@test.com",
+      password: password,
+      password_confirmation: password
+  )
+end
